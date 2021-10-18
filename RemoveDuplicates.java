b4f4593c053d8d4090d5888182ffbf579cdb5bd3
Aravindh55 <91793881+Aravindh55@users.noreply.github.com>
2021-10-18 22:38:29 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,36 @@
+package week3.day2;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates {
+
+	public static void main(String[] args) {
+		String company = "PayPal India";
+		char[] charArray = company.toCharArray();
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		Set<Character> dupSet = new LinkedHashSet<Character>();
+		for (int i = 0; i < charArray.length; i++) {
+			if (!charSet.add(charArray[i])) {
+				dupSet.add(charArray[i]);
+			}
+			if (dupSet.contains(charArray[i])) {
+				charSet.remove(charArray[i]);
+			}
+		}
+		System.out.println("before removing duplicates");
+		System.out.println(company);
+		System.out.println("after removing the duplicates");
+		for (Character character : charSet) {
+			if (!charSet.isEmpty()) {
+				System.out.print(character);
+			} else {
+				System.out.println("the charset is empty");
+			}
+		}
+
+	}
+
+}
